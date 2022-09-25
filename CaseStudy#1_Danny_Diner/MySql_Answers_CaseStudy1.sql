5bf0752a13249bbdfdf3655130f9a41f942a4558
MelanieFR <melanie.sanctorum.pro@gmail.com>
2022-09-25 18:36:38 -0400

    My_sql_answers_part2

--- CaseStudy#1_Danny_Diner/MySql_Answers_CaseStudy1.sql
+++ CaseStudy#1_Danny_Diner/MySql_Answers_CaseStudy1.sql
@@ -1,4 +1,4 @@
--- What is the total amount each customer spent at the restaurant?
+-- 1. What is the total amount each customer spent at the restaurant?
 SELECT distinct(s.customer_id), concat('$', sum(price)) as total_spending
 FROM sales as s
 INNER JOIN menu as m
@@ -7,15 +7,15 @@ GROUP by s.customer_id
 ORDER BY s.customer_id ASC
 ;
 
--- How many days has each customer visited the restaurant?
+-- 2. How many days has each customer visited the restaurant?
 SELECT distinct(customer_id), count(distinct order_date) as nbr_of_visits
 FROM sales
 GROUP BY customer_id
 ORDER BY customer_id ASC
 ;
 
--- What was the first item from the menu purchased by each customer?
--- This kind of question can be answered with a window function, because it requires to "order" the values based on the order_date for each customer
+-- 3. What was the first item from the menu purchased by each customer?
+-- a. This kind of question can be answered with a window function, because it requires to "order" the values based on the order_date for each customer
 WITH CTE as (
 SELECT customer_id, product_name, DENSE_RANK() OVER (Partition by customer_id Order by order_date) as rnk
 -- even though values are ordered ASC by default, it is good practice to write it down for the window functions
@@ -45,8 +45,8 @@ GROUP BY customer_id
 ORDER BY customer_id ASC
 ;
 
--- That question can also be answered by using the MIN() function on order_date:
-SELECT s.customer_id, group_concat(m.product_name)
+-- b. That question can also be answered by using the MIN() function on order_date:
+SELECT s.customer_id, s.order_date, group_concat(m.product_name) as first_order
 FROM sales as s
 JOIN menu as m
 USING(product_id)
@@ -56,19 +56,49 @@ FROM sales
 GROUP BY customer_id)
 as sub
 ON s.customer_id = sub.customer_id AND s.order_date = sub.min_date
-GROUP BY 
-s.customer_id ASC
+GROUP BY s.customer_id
+ORDER BY s.customer_id
 
+-- Explanation:
 -- In the subquery, I apply the min() function on the order_date column to get only the first order date for all customers
 -- Then I group my min_date rows PER customer_id
 -- Then I join the outer query with the suquery on a common column AND on the SAME order_date to filter in only these dates 
 -- Finally I group by customer_id 
 ;
 
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+SELECT product_name, count(order_date) as most_purchased_item
+FROM sales as s
+INNER JOIN menu as m
+USING (product_id)
+GROUP BY product_name
+ORDER BY cnt DESC
+LIMIT 1
+;
+
+-- 5. Which item was the most popular for each customer?
+-- I can use a window function to "order" the items per nbr of times they were ordered by each customer 
+WITH ordering AS (
+SELECT customer_id, product_name, count(product_id) as cnt, DENSE_RANK() OVER(partition by customer_id order by count(order_date) DESC) as rnk
+FROM sales as s
+INNER JOIN menu as m
+USING (product_id)
+GROUP BY customer_id, product_name
+)
+SELECT customer_id, group_concat(product_name)
+FROM ordering
+WHERE rnk = 1
+GROUP BY customer_id;
+
+-- I would use the column 'cnt' to show how many times the most popular item per customer has been ordered
+-- Also, add the group by 'product_name' to be sure to return all popular items (in the case there are several - see customer B)
+SELECT customer_id, product_name, cnt
+FROM ordering
+WHERE rnk = 1
+GROUP BY customer_id, product_name
 
 
--- What is the most purchased item on the menu and how many times was it purchased by all customers?
--- Which item was the most popular for each customer?
+
 -- Which item was purchased first by the customer after they became a member?
 -- Which item was purchased just before the customer became a member?
 -- What is the total items and amount spent for each member before they became a member?
