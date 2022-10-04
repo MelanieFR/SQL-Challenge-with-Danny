600b40b44db5a6fa8ac85c9eec998e26e8070951
MelanieFR <melanie.sanctorum.pro@gmail.com>
2022-10-04 19:19:51 -0400

    CaseStudy2_cleaning

---
+++ CaseStudy#2_Pizza_Runner/MySQL_data_cleaning.sql
@@ -0,0 +1,30 @@
+-- Proceed to data cleaning first:
+
+SELECT *
+FROM customer_orders;
+
+-- The exclusion and extras columns have 'null' values, empty strings (' ') which are not considered NULL by SQL
+-- hence we need to clean these columns before creating queries
+
+DROP TABLE IF EXISTS customer_orders_clean;
+
+CREATE TABLE customer_orders_clean AS
+SELECT order_id,
+       customer_id,
+       pizza_id,
+       CASE
+           WHEN exclusions = '' THEN NULL
+           WHEN exclusions = 'null' THEN NULL
+           ELSE exclusions
+       END AS exclusions,
+       CASE
+           WHEN extras = '' THEN NULL
+           WHEN extras = 'null' THEN NULL
+           ELSE extras
+       END AS extras,
+       order_time
+FROM customer_orders;
+
+SELECT * FROM customer_orders_clean;
+
+
