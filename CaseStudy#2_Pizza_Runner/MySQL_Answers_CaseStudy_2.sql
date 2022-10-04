d0ba0fc0490cea778f04e14303825af284106f83
MelanieFR <melanie.sanctorum.pro@gmail.com>
2022-10-04 19:49:34 -0400

    CaseStudy2

--- CaseStudy#2_Pizza_Runner/MySQL_Answers_CaseStudy_2.sql
+++ CaseStudy#2_Pizza_Runner/MySQL_Answers_CaseStudy_2.sql
@@ -10,23 +10,23 @@ FROM customer_orders;
 -- 3. How many successful orders were delivered by each runner?
 	-- the cancellation column will give me the reason why a customer cancelled the order. So, applying the inverse logic I'll be able to identify the delivered orders
 SELECT runner_id, count(distinct order_id) as nbr_customer_orders
-FROM runner_orders as r
-WHERE cancellation LIKE '%null%' OR cancellation IS NULL OR cancellation LIKE '% %'
+FROM runner_orders_clean
+WHERE cancellation IS NULL
 GROUP BY runner_id
 ;
 
 -- 4. How many of each type of pizza was delivered?
 
-SELECT pizza_name as pizza_type, count(pizza.pizza_id) as delivered
+SELECT p.pizza_id, pizza_name as pizza_type, count(pizza.pizza_id) as delivered
 FROM (
 SELECT  pizza_id
-FROM customer_orders as c
-INNER JOIN runner_orders as r
+FROM customer_orders_clean as c
+INNER JOIN runner_orders_clean as r
 ON c.order_id = r.order_id
-WHERE cancellation LIKE '%null%' OR cancellation IS NULL OR cancellation LIKE '% %') as pizza
+WHERE cancellation IS NULL) as pizza
 INNER JOIN pizza_names as p
 ON pizza.pizza_id = p.pizza_id
-GROUP BY pizza_name
+GROUP BY p.pizza_id, pizza_name
 ORDER BY pizza_type
 
 
